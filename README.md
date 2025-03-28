a6a34c98ce0d81754f3214b05761bb081e09f767
Yu270 <yuhanmenard@gmail.com>
2025-03-28 10:23:00 -0400

    embeddings pré-entrainés

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Projet de traitement automatique du langage
 Ce répertoire contient tous les scripts nécessaires au projet.  
 * **donnees** : scripts de gestion des données 
   * *nettoyage.py* : fonctions de nettoyage des données
+  * *utils.py* : fonctions utilitaires pour manipuler les données pré-entrainement
+* **embedding.py** : fonctions d'embedding des données
+* **main.py** : script principal
+* **exemple_pretraitement** : notebook qui présente un exemple de pré-traitement
---
+++ donnees/utils.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class FakeNewsDataset(Dataset):
+    def __init__(self, X: torch.Tensor, y: torch.Tensor):
+        self.X = X.clone()
+        self.y = y.clone()
+
+    def __len__(self):
+        return self.y.shape[0]
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+
+def ajuster_canaux(X: torch.Tensor, nb_canaux: int):
+    """
+    Fonction qui ajuste le nombre de canaux d'un tenseur. 
+
+    Entrées
+        X: tenseur (i, j, k)
+        nb_canaux: nombre de canaux désirés
+    
+    Sortie
+        X ajusté (i, nb_canaux, k)
+    """
+    with torch.no_grad():
+        manque = nb_canaux-X.shape[1]
+        if manque>0:
+            return torch.cat((X,torch.zeros((X.shape[0],manque,X.shape[2]))),1)
+        elif manque<0:
+            return X[:,:manque,:].clone()
+        else:
+            return X.clone()
