34c5b4954214d6105e69e678f38fad38c5f0c682
Yu270 <yuhanmenard@gmail.com>
2025-04-18 12:13:16 -0400

    compléter la documentation

--- predict.py
+++ predict.py
@@ -3,13 +3,14 @@ from bert.predire import predire_binaire
 
 
 def argument_parser():
-    parser = argparse.ArgumentParser(usage='\n python predict.py [model] [model_dir] [data_path] [dataset]'
-                                           '\n python predict.py --model=true --model_dir=./modeles/bert_true --data_path=./donnees/FakeNews_Task3_2022_V0/English_data_test_release_with_rating.csv --dataset=test',
+    parser = argparse.ArgumentParser(usage='\n python predict.py [model] [data_path] [dataset] [other dir]'
+                                            '\n python predict.py --model=true --data_path=./donnees/FakeNews_Task3_2022_V0/English_data_test_release_with_rating.csv --dataset=test'
+                                            '\n python predict.py --model=true --model_dir=./modeles/bert_true --data_path=./donnees/FakeNews_Task3_2022_V0/English_data_test_release_with_rating.csv --dataset=test',
                                      description="Ce script permet d'obtenir des prédictions des différents modèles de classification binaire.",
                                      add_help=True)
     parser.add_argument('--model', type=str, default="None", choices=["None", "false", "other", "partfalse", "true"],
                         help="Tâche de classification")
-    parser.add_argument('--model_dir', type=str, default="None",
+    parser.add_argument('--model_dir', type=str, default="./modeles/bert",
                         help="Chemin du modèle.")
     parser.add_argument('--data_path', type=str, default="None",
                         help="Chemin des données.")
@@ -26,9 +27,6 @@ if __name__ == "__main__":
 
     if args.model=="None":
         print("Aucun modèle spécifié")
-        exit(0)
-    if args.model_dir=="None":
-        print("Aucun chemin de modèle spécifié")
         exit(0)
     if args.data_path=="None":
         print("Aucune données spécifiées")
